a3c77c3835a4ffb42a03d5562ad946b151293f40
Dean Vaturi <dean1990@gmail.com>
2023-04-16 20:33:17 +0000

    changed few files

--- jenkinsFiles/deploy_app.groovy
+++ jenkinsFiles/deploy_app.groovy
@@ -1,12 +1,12 @@
 node('slave1 || slave2') {
-    stage('clone git'){
+    stage('clone git repo'){
         git branch: 'main', changelog: false, credentialsId: 'github', poll: false, url: 'git@github.com:dvaturi/Opsschool-project.git'
     }
     
     stage('deploy app'){
             sh '''     
-                kubectl apply -f  ./Opsschool-project/kubeFiles/kandula_deploy.yaml
-                kubectl apply -f  ./Opsschool-project/kubeFiles/kandula_service.yaml
+                kubectl apply -f  ./kubeFiles/kandula_deploy.yaml
+                kubectl apply -f  ./kubeFiles/kandula_service.yaml
             '''
     }
 }
