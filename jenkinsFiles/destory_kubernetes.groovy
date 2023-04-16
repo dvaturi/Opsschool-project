6699d46ce33ec5ac0e3a758113aa8e5205083b0a
Dean Vaturi <dean1990@gmail.com>
2023-04-16 22:24:55 +0000

    modified destroy

--- jenkinsFiles/destory_kubernetes.groovy
+++ jenkinsFiles/destory_kubernetes.groovy
@@ -13,8 +13,8 @@ node('slave1 || slave2') {
     stage("uninstall kandula service"){
         sh '''
             echo "deleting kandula service"
-            if kubectl get svc kandula-service; then
-                kubectl delete -f ./kubeFiles/kandula_service.yaml
+            if kubectl get svc kandula-service -n your-namespace; then
+                kubectl delete svc kandula-service -n your-namespace
             else
                 echo "kandula service is not available"
             fi
@@ -24,8 +24,8 @@ node('slave1 || slave2') {
     stage("uninstall kandula deployment") {
         sh '''
             echo "deleting kandula deployment"
-            if kubectl get deploy kandula-deployment; then
-                kubectl delete -f ./kubeFiles/kandula_deploy.yaml
+            if kubectl get deploy kandula-deployment -n your-namespace; then
+                kubectl delete deploy kandula-deployment -n your-namespace
             else
                 echo "kandula deployment is not available"
             fi
