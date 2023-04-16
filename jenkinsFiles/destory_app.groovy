c558ef7cbbafb537b12c045a270cea73a1a88a10
Dean Vaturi <dean1990@gmail.com>
2023-04-16 23:16:08 +0000

    modified destroy

--- jenkinsFiles/destory_app.groovy
+++ jenkinsFiles/destory_app.groovy
@@ -17,7 +17,7 @@ pipeline {
             }
         }
         stage('clone git repo'){
-            agent { label currentBuild.agentLabel }
+            agent {slave1}
             steps {
                 git branch: 'main', changelog: false, credentialsId: 'github', poll: false, url: 'git@github.com:dvaturi/Opsschool-project.git'
             }
