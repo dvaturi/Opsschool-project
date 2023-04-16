a7c335325c38a3cc49f9609fd64d023112dcd85d
Dean Vaturi <dean1990@gmail.com>
2023-04-16 21:00:06 +0000

    modified destroy

--- jenkinsFiles/destory_kubernetes.groovy
+++ jenkinsFiles/destory_kubernetes.groovy
@@ -3,11 +3,18 @@ node('slave1 || slave2') {
         git branch: 'main', changelog: false, credentialsId: 'github', poll: false, url: 'git@github.com:dvaturi/Opsschool-project.git'
     }
 
+    stage("update kubeconfig"){
+        sh '''
+            echo "updating kubeconfig"
+            aws eks --region=us-east-1 update-kubeconfig --name ${params.CLUSTER_NAME}
+        '''   
+    }
+
     stage("uninstall kondula app"){
         sh '''
             echo "deleting service & app pods"
-            kubectl delete -f .kubeFiles/kandula_service.yaml
-            kubectl delete -f .kubeFiles/kandula_deploy.yaml
+            kubectl delete -f ./kubeFiles/kandula_service.yaml
+            kubectl delete -f ./kubeFiles/kandula_deploy.yaml
         '''   
     }
 
