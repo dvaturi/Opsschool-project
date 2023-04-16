ff8cb3d145beae1ac0a354185fe8cdb29c68a1f6
Dean Vaturi <dean1990@gmail.com>
2023-04-16 23:23:33 +0000

    modified destroy

--- jenkinsFiles/destory_app.groovy
+++ jenkinsFiles/destory_app.groovy
@@ -4,41 +4,34 @@ node('slave1 || slave2') {
     }
 
     stage("update kubeconfig"){
-        agent { label currentBuild.agentLabel }
-        steps {
-            sh """
-                echo 'updating kubeconfig'
-                aws eks --region=us-east-1 update-kubeconfig --name ${params.CLUSTER_NAME}
-            """
-        }
+        sh """
+            echo 'updating kubeconfig'
+            aws eks --region=us-east-1 update-kubeconfig --name ${params.CLUSTER_NAME}
+        """
     }
+    
     stage("uninstall kandula service"){
-        agent { label currentBuild.agentLabel }
-        steps {
-            sh '''
-                echo "deleting kandula service"
-                if kubectl get svc backend-service -n kandula; then
-                kubectl delete -f ./kubeFiles/kandula_service.yaml -n kandula
-                else
-                    echo "kandula service is not available"
-                fi
-            '''
-        }
+        sh '''
+            echo "deleting kandula service"
+            if kubectl get svc backend-service -n kandula; then
+            kubectl delete -f ./kubeFiles/kandula_service.yaml -n kandula
+            else
+                echo "kandula service is not available"
+            fi
+        '''
     }
     stage("uninstall kandula deployment") {
-        agent { label currentBuild.agentLabel }
-        steps {
-            sh '''
-                echo "deleting kandula deployment"
-                if kubectl get deploy kandula-prod -n kandula; then
-                kubectl delete -f ./kubeFiles/kandula_deploy.yaml  -n kandula
-                else
-                    echo "kandula deployment is not available"
-                fi
-            '''
-        }
+        sh '''
+            echo "deleting kandula deployment"
+            if kubectl get deploy kandula-prod -n kandula; then
+            kubectl delete -f ./kubeFiles/kandula_deploy.yaml  -n kandula
+            else
+                echo "kandula deployment is not available"
+            fi
+        '''
     }
 }
+
 
 post {
     success {
