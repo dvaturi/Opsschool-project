885d9e2db5dc781a2872551b0ef37be3ba248fc6
Dean Vaturi <dean1990@gmail.com>
2023-04-16 21:18:05 +0000

    modified destroy

--- jenkinsFiles/deploy_app.groovy
+++ jenkinsFiles/deploy_app.groovy
@@ -2,11 +2,18 @@ node('slave1 || slave2') {
     stage('clone git repo'){
         git branch: 'main', changelog: false, credentialsId: 'github', poll: false, url: 'git@github.com:dvaturi/Opsschool-project.git'
     }
+
+    stage("update kubeconfig"){
+        sh """
+            echo 'updating kubeconfig'
+            aws eks --region=us-east-1 update-kubeconfig --name ${params.CLUSTER_NAME}
+        """   
+    }
     
     stage('deploy app'){
-            sh '''     
-                kubectl apply -f  ./kubeFiles/kandula_deploy.yaml
-                kubectl apply -f  ./kubeFiles/kandula_service.yaml
-            '''
+        sh '''     
+            kubectl apply -f  ./kubeFiles/kandula_deploy.yaml
+            kubectl apply -f  ./kubeFiles/kandula_service.yaml
+        '''
     }
 }
--- jenkinsFiles/destory_kubernetes.groovy
+++ jenkinsFiles/destory_kubernetes.groovy
@@ -10,13 +10,25 @@ node('slave1 || slave2') {
         """   
     }
 
-
-
-    stage("uninstall kondula app"){
+    stage("uninstall kandula service"){
         sh '''
-            echo "deleting service & app pods"
-            kubectl delete -f ./kubeFiles/kandula_service.yaml
-            kubectl delete -f ./kubeFiles/kandula_deploy.yaml
+            echo "deleting kandula service"
+            if kubectl get svc kandula-service; then
+                kubectl delete -f ./kubeFiles/kandula_service.yaml
+            else
+                echo "kandula service is not available"
+            fi
+        '''   
+    }
+
+    stage("uninstall kandula deployment") {
+        sh '''
+            echo "deleting kandula deployment"
+            if kubectl get deploy kandula-deployment; then
+                kubectl delete -f ./kubeFiles/kandula_deploy.yaml
+            else
+                echo "kandula deployment is not available"
+            fi
         '''   
     }
 
