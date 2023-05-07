fd8147265acd62b17fbf0e34b4328ecbcc724a3f
Dean Vaturi <dean1990@gmail.com>
2023-05-07 00:24:02 +0000

    modified

--- terraform/2.environment/lb_private.tf
+++ terraform/2.environment/lb_private.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "private_services" {
   name               = "privateservices"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.consul_sg.id, aws_security_group.jenkins_sg.id]
+  security_groups    = [aws_security_group.consul_sg.id, aws_security_group.jenkins_sg.id, aws_security_group.prometheus_sg.id , aws_security_group.elk_sg.id ]
   subnets            = module.vpc_module.public_subnets_id
 
   tags = {
