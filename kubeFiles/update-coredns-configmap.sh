85a7226d7f861cdbe9f7104425190046c6bf26d3
Dean Vaturi <dean1990@gmail.com>
2023-06-24 22:50:57 +0000

    modified

--- kubeFiles/update-coredns-configmap.sh
+++ kubeFiles/update-coredns-configmap.sh
@@ -18,17 +18,7 @@ fi
 current_config=$(kubectl get configmap coredns -n kube-system -o yaml)
 
 # Update the Corefile section in the current ConfigMap YAML
-updated_config=$(echo "$current_config" | awk -v consul_dns_ip="$consul_dns_ip" '/Corefile:/{flag=1;print;next}/^kind:/{flag=0}flag{print}END{print "    consul {\n        errors\n        cache 30\n        forward . " consul_dns_ip "\n    }"}')
+updated_config=$(echo "$current_config" | awk 'NR==18{print "    consul {\n        errors\n        cache 30\n        forward . '"$consul_dns_ip"'\n    }"}1')
 
 # Apply the updated ConfigMap
 echo "$updated_config" | kubectl apply -f - -n kube-system
-
-# Create a temporary file with the updated ConfigMap YAML
-updated_configmap_file=$(mktemp)
-echo "$current_config" | awk -v updated_corefile="$updated_corefile" '/Corefile:/{flag=1;print;next}/^kind:/{flag=0}flag{print updated_corefile}' > "$updated_configmap_file"
-
-# Apply the updated ConfigMap
-kubectl apply -f "$updated_configmap_file" -n kube-system
-
-# Clean up the temporary file
-rm "$updated_configmap_file"
