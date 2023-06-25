2852ab9457937ece0a2e6375d8bfcdc77bec0271
Dean Vaturi <dean1990@gmail.com>
2023-06-25 00:33:20 +0000

    modified

--- jenkinsFiles/install_consul_k8s.groovy
+++ jenkinsFiles/install_consul_k8s.groovy
@@ -20,15 +20,11 @@ pipeline {
 
         stage('Install Cosnul on Kubernetes'){
             steps {
-                
                 sh """ 
                     echo 'Install consul'
                     helm repo add hashicorp https://helm.releases.hashicorp.com
                     kubectl create secret generic consul-gossip-encryption-key --from-literal=key="uDBV4e+LbFW3019YKPxIrg==" --namespace consul
                     chmod 600 /home/ubuntu/.kube/config
-                    output=$(dig +answer consul.service.opsschool.consul:8600)
-                    ip=$(echo "$output" | grep -oE 'SERVER: ([^#]+)' | awk '{print $NF}')
-                    sed -i "s|<consul-server-ip>|$ip|g" ./kubeFiles/values_consul.yaml
                     helm install --values ./kubeFiles/values_consul.yaml consul hashicorp/consul  --namespace consul
                 """
                 
