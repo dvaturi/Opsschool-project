09185e5b84c9d03bfd7686d307dd948e78cd9e6d
Dean Vaturi <dean1990@gmail.com>
2023-06-25 14:37:09 +0000

    modified

--- jenkinsFiles/build_app.groovy
+++ jenkinsFiles/build_app.groovy
@@ -34,8 +34,8 @@ pipeline {
                 if (var != 0){
                     echo "Vunerability found!"
                 }
-                }
             }
+        }
         stage('run docker'){
             steps {
                 script {
