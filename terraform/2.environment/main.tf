305ca4d3ea97751dd2c46dd36ebefd9dfc61b8ee
Dean Vaturi <dean1990@gmail.com>
2023-06-26 12:57:44 +0000

    modified

--- terraform/2.environment/main.tf
+++ terraform/2.environment/main.tf
@@ -1,6 +1,6 @@
 module "vpc_module" {
   source                    = "./modules/vpc"
   vpc_cidr_block            = "10.0.0.0/16"
-  private_subnets_cidr_list = ["10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets_cidr_list  = ["10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets_cidr_list = ["10.0.0.0/20", "10.0.16.0/20"]
+  public_subnets_cidr_list  = ["10.0.32.0/20", "10.0.48.0/20"]
 }
